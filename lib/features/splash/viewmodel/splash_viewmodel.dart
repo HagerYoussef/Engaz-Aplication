afa12b64d01f6dd42cedd6b7a27ee8b26965255f
HagerYoussef <hageryossef5@gmail.com>
2025-04-08 16:34:26 +0200

    Splach Screen

---
+++ lib/features/splash/viewmodel/splash_viewmodel.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+class SplashViewModel extends ChangeNotifier {
+  Future<void> handleStartup(BuildContext context) async {
+    await Future.delayed(const Duration(seconds: 3));
+
+    if (context.mounted) {
+      Navigator.pushReplacementNamed(context, '/login');
+    }
+  }
+}
